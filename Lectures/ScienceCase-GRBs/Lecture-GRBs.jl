b59d5ee1d76c7eae2fb7cef74eeb8013d481d06c
Stefano Covino <stefano.covino@inaf.it>
2026-04-10 06:32:39 +0200

    100426 commit

--- Lectures/ScienceCase-GRBs/Lecture-GRBs.jl
+++ Lectures/ScienceCase-GRBs/Lecture-GRBs.jl
@@ -27,12 +27,7 @@ md"""
 **What is this?**
 
 
-*This jupyter notebook is part of a collection of notebooks on various topics discussed during the Time Domain Astrophysics course delivered by Stefano Covino at the [Università dell'Insubria](https://www.uninsubria.eu/) in Como (Italy). Please direct questions and suggestions to [stefano.covino@inaf.it](mailto:stefano.covino@inaf.it).*
-"""
-
-# ╔═╡ ec67de24-d88a-46fa-ae46-c0cd7b797adc
-md"""
-**This is a `Pluto` notebook**
+*This notebook is part of a collection of `pluto` notebooks on various topics discussed during the Time Domain Astrophysics course delivered by Stefano Covino at the [Università dell'Insubria](https://www.uninsubria.eu/) in Como (Italy). Please direct questions and suggestions to [stefano.covino@inaf.it](mailto:stefano.covino@inaf.it).*
 """
 
 # ╔═╡ 3ddd0f61-79d0-473c-8fec-a0e0c3fc72bf
@@ -793,16 +788,19 @@ cm"""
 	<td></td>
     <td>Previous lecture</td>
     <td>Next lecture</td>
+	<td>Course Summary</td>	
   </tr>
   <tr>
 	<td>notebook</td>
     <td><a href="./open?path=Lectures/Lecture-NonParametricAnalysis/Lecture-NonParametricPeriodograms.jl">Lecture about non-parametric periodograms</a></td>
     <td><a href="./open?path=Lectures/Lecture-SingularSpectrumAnalysis/Lecture-SSA.jl">Lecture about singlular spectrum analysis"</a></td>
+	<td><a href="./open?path=Course.jl">Course Summary</a></td>    
   </tr>
   <tr>
 	<td>html</td>
     <td><a href="Lectures/Lecture-NonParametricAnalysis/Lecture-NonParametricPeriodograms.html">Lecture about non-parametric periodograms</a></td>
 <td><a href="Lectures/Lecture-SingularSpectrumAnalysis/Lecture-SSA.html">Lecture about singlular spectrum analysis</a></td>
+	<td><a href="../../Course.html">Course Summary</a></td>    
   </tr>
 </table>
 
@@ -815,6 +813,9 @@ md"""
 
 This notebook is provided as [Open Educational Resource](https://en.wikipedia.org/wiki/Open_educational_resources). Feel free to use the notebook for your own purposes. The text is licensed under [Creative Commons Attribution 4.0](https://creativecommons.org/licenses/by/4.0/), the code of the examples, unless obtained from other properly quoted sources, under the [MIT license](https://opensource.org/licenses/MIT). Please attribute the work as follows: *Stefano Covino, Time Domain Astrophysics - Lecture notes featuring computational examples, 2026*.
 """
+
+# ╔═╡ 0eaec6b9-82a6-4166-a8bd-c3c8147238d8
+md"Notebook v1.0.0 - 10 April 2026"
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -2735,9 +2736,8 @@ version = "4.1.0+0"
 
 # ╔═╡ Cell order:
 # ╟─4d477519-c44f-434c-b7e0-8daaa5009358
-# ╟─ec67de24-d88a-46fa-ae46-c0cd7b797adc
-# ╠═6a1315d1-9a6d-4ce0-b1c0-3fe22beb1ec2
-# ╠═7f04786b-a1c6-42ac-ac41-2cbe68878149
+# ╟─6a1315d1-9a6d-4ce0-b1c0-3fe22beb1ec2
+# ╟─7f04786b-a1c6-42ac-ac41-2cbe68878149
 # ╟─3ddd0f61-79d0-473c-8fec-a0e0c3fc72bf
 # ╟─5029a214-0841-40fb-b397-4a2e1047bfb7
 # ╟─404060d3-23ec-400b-84cf-779e63b90293
@@ -2788,5 +2788,6 @@ version = "4.1.0+0"
 # ╟─3adba216-1df6-43f0-8000-05084c4d58c2
 # ╟─b36fd613-95c8-44bf-876d-4eb345c26f08
 # ╟─206474b8-0811-4785-8a71-acdcfd20b76c
+# ╟─0eaec6b9-82a6-4166-a8bd-c3c8147238d8
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This is a repository with material (pluto notebooks, papers, etc.) for the **Tim
 
 An online version of the course can be accessed [here](http://192.167.38.133:8081/Course.html).
 
-*Last update: 09 Apr 2026.*
+*Last update: 10 Apr 2026.*
